6dcbe7a230cc70cae6f4f510091f15ae83e662e1
nicolasbaezmora0 <nicolas.baez@segurosbolivar.com>
2024-03-26 14:31:29 -0500

    Se ajustan bugs

--- src/main/java/com/proyectoTC/Taller_17_TC/utils/PaymentType.java
+++ src/main/java/com/proyectoTC/Taller_17_TC/utils/PaymentType.java
@@ -1,15 +1,18 @@
 package com.proyectoTC.Taller_17_TC.utils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PaymentType {
 
-    private static final Map<Integer, String> types = Map.of(
-            1, "EFECTIVO",
-            2, "TARJETA DE CREDITO",
-            3, "TARJETA DEBITO"
+    private static final Map<Integer, String> types = new HashMap<>();
 
-    );
+    static {
+        types.put(1, "EFECTIVO");
+        types.put(2, "TARJETA DE CREDITO");
+        types.put(3, "TARJETA DEBITO");
+    }
+
     public static String getTypePay(int id) {
         if (types.get(id) == null) return "Otro";
         return types.get(id);
